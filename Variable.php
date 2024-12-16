723f298e0172018a18e92ed1d4bdb0e39ad51754
Nurul Islam <nurul.cse7@gmail.com>
2024-12-16 15:26:45 +0600

    first commit

---
+++ Variable.php
@@ -0,0 +1,15 @@
+<?php
+ // Data Types / Variable Type 
+
+// Predefined Data Types
+Integer 
+Float (Double)
+String 
+Boolean
+
+// User-Defined (compound) Data Types
+Array 
+Objects
+
+// Special Data Types
+NULL
---
+++ integer.php
@@ -0,0 +1,23 @@
+<?php
+echo "hello php"; 
+
+
+// decimal base integers
+$deci1 = 50; 
+$deci2 = 654; 
+
+// octal base integers
+$octal1 = 07; 
+
+// hexadecimal base integers
+$octal = 0x45; 
+
+$sum = $deci1 + $deci2;
+echo $sum;
+echo "\n\n"; // Line break 
+
+//returns data type and value
+var_dump($sum) 
+
+?>
+
